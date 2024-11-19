ca48c260d5a082d5749f869713ce7db7d3729cf7
Amir Khan <amirnaiyarkhan@gmail.com>
2024-11-19 14:07:30 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,42 @@ import { useUser } from '@auth0/nextjs-auth0/client';
 import FlexBox from './components/FlexBox';
 import TextField from './components/TextField';
 import Image from 'next/image';
-import Button from 'components/Button';
-import { useRouter } from 'next/navigation';
+// import Button from 'components/Button';
+// import { useRouter } from 'next/navigation';
+// import { useEffect } from 'react';
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
-  const router = useRouter();
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
+  // const getData = async () => {
+  //   const request = new Request(
+  //     'https://devecom-80zu255o.b4a.run/auth/signup',
+  //     {
+  //       method: 'POST',
+  //       body: JSON.stringify({
+  //         email: '2motor@gmail.com',
+  //         name: {
+  //           firstName: 'Tata',
+  //           lastName: 'Motors',
+  //         },
+  //         password: 'Motors@123',
+  //       }),
+  //       headers: {
+  //         'X-Parse-Application-Id': 'BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f', // This is the fake app's application id
+  //         'X-Parse-REST-API-Key': 'swrFFIXJlFudtF3HkZPtfybDFRTmS7sPwvGUzQ9w', // This is the fake app's readonly master key
+  //         'X-Parse-Revocable-Session': '1',
+  //       },
+  //     }
+  //   );
+  //   const response1 = await fetch(request);
+  //   console.log(response1.status, 'response1');
+  // };
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
   return (
@@ -19,7 +49,7 @@ export default function Home() {
       alignItems={'center'}
       flexDirection={'column'}
     >
-      <TextField as="h1">Coming Soon Cricket Australia</TextField>
+      <TextField as="h1">Coming Soon </TextField>
       {user && (
         <div>
           <Image
@@ -33,6 +63,8 @@ export default function Home() {
           <TextField as="p">{user.email}</TextField>
         </div>
       )}
+      {/*
+
       <FlexBox flexDirection="column">
         {!user ? (
           <Button
@@ -52,6 +84,7 @@ export default function Home() {
           />
         )}
       </FlexBox>
+      */}
     </FlexBox>
   );
 }
